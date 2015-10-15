9ed9d5f1230a93c316a7023c8f06c45b167e54ad
Lennart <lennart@getbynder.com>
2015-10-15 10:13:15 +0200

    Update 070.py

--- plugins/070.py
+++ plugins/070.py
@@ -1,17 +1,12 @@
-import requests,json,HTMLParser,sys,getopt
+import requests,json,HTMLParser
 
 from will.plugin import WillPlugin
 from will.decorators import respond_to
 
 class NulZeventigPlugin(WillPlugin):
-    @respond_to("070")
-    def nul_zeventig_plugin(self, message, argv):
-		if len(argv) > 1:
-			if len(sys.argv[1]) > 0:
-				r = requests.get("http://looq.nl/070/index.php?text="+"{}".format("+".join(sys.argv)))
-			else:
-				r = requests.get("http://looq.nl/070/index.php?text=Den Haag is de mooiste stad van de wereld")
-		else:
-			r = requests.get("http://looq.nl/070/index.php?text=Den Haag is de mooiste stad van de wereld")
+	@respond_to("070 (?<text>.*)")
+	def nul_zeventig_plugin(self, message, text='Den Haag is de mooiste stad van de wereld'):
+		h = HTMLParser.HTMLParser()
+		r = requests.get("http://looq.nl/070/index.php?text="+"{}".format("+".join(text)))
 		parsed_json = json.loads(r.content)
 		return(h.unescape(parsed_json['translation']))
