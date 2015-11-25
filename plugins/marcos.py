556341b2b723e5d16f89ac38d76a5b84a41ba88b
Huib Piguillet <hpiguillet@gmail.com>
2015-11-25 17:18:52 +0100

    Regex interpunction improvement

--- plugins/marcos.py
+++ plugins/marcos.py
@@ -3,7 +3,7 @@ from will.decorators import respond_to, hear
 
 
 class MarcosPlugin(WillPlugin):
-    @hear("(^|\s)pan($|\s)")
+    @hear("(^|[^a-zA-Z])pan($|[^a-zA-Z])")
     @respond_to("marcos")
     def get_marcos(self, message):
         """
