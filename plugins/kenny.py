682fb0f92b2cf9513e25362fbb809c7346444618
Lennart <lennart@getbynder.com>
2016-02-10 22:17:08 +0100

    Update kenny.py

--- plugins/kenny.py
+++ plugins/kenny.py
@@ -4,10 +4,10 @@ from will.plugin import WillPlugin
 from will.decorators import respond_to
 
 class KennyPlugin(WillPlugin):
-    @respond_to("what does kenny think?")
+    @hear("kenny?")
     def get_kenny(self, message):
         """
-        what does kenny think?
+        kenny?: gives an honest reply from kenny powers
         """
         self.reply(message,
                    requests.get("http://www.looq.nl/kennybot/index.php").content)
